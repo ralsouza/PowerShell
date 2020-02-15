26c388b7172d07b2410f78cc136de8b74a7653c6
Rafael Lima <ls.rafael@live.com>
2020-02-15 12:22:49 -0300

    Update check_perfmon_status.ps1

--- check_perfmon_status.ps1
+++ check_perfmon_status.ps1
@@ -10,9 +10,12 @@ $datacollectorset = New-Object -COM Pla.DataCollectorSet
 $datacollectorset.Query("Baseline SQL Server",$null)
 
 if ($datacollectorset.Status -eq 0) {
+
 	Write-Output "$(Get-TimeStamp) The performance counter is stopped. Trying to start..." | Out-file C:\Logs\SQL_Perf\perf_sql_status_log.txt -append
+	
 	$datacollectorset.Start($false)
+	
 	Write-Output "$(Get-TimeStamp) It started successfully!" | Out-file C:\Logs\SQL_Perf\perf_sql_status_log.txt -append
 }else{
-		Write-Output "$(Get-TimeStamp) Performance counter is ok, nothing to do." | Out-file C:\Logs\SQL_Perf\perf_sql_status_log.txt -append
+	Write-Output "$(Get-TimeStamp) Performance counter is ok, nothing to do." | Out-file C:\Logs\SQL_Perf\perf_sql_status_log.txt -append
 	}
